fc0a947b418f11261e610719e5fdcd6d24485068
Murilo Silva <murilo.alex.silva@gmail.com>
2021-04-22 16:18:54 -0300

    Iniciando o app

--- todo_list/lib/app/views/home_page.dart
+++ todo_list/lib/app/views/home_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io'; //File
 import 'dart:async'; //Future
-import 'dart:convert';
-import 'dart:async';
+import 'dart:convert'; //Arquivos json
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -11,13 +10,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List toDoList = [];
+  List _toDoList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo List'),
+        title: Text('Lista de Tarefas'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 17, top: 1, right: 7, bottom: 1),
+            child: Row(
+              children: [
+                Expanded(
+                  //Necessitamos colocar o expanded para o app saber qual o
+                  //tamanho do textField, assim ele irá colocar o textField
+                  //no maximo que ele puder.
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  color: Colors.blueAccent,
+                  child: Text('ADD'),
+                  textColor: Colors.white,
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -29,5 +58,26 @@ class _HomePageState extends State<HomePage> {
     no ato, utilizamos a função assincrona*/
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    /*Aqui estamos pegando a todoList transformando em json e armazenando
+    em uma String com o nome de data
+    */
+    String data = jsonEncode(_toDoList);
+    //Precisamos colocar o await pq o getFile é um valor futuro
+    final file = await _getFile();
+    //Aqui iremos escrever o arquivo data em String no file
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    //O try catch irá tentar exibir algo (try) e caso de errado irá exibir o erro descrito em catch
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
